89c07544457c78afab3774963dab205dacca32ea
Jim Burbridge <jhechtf@gmail.com>
2018-03-26 17:43:17 -0700

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+//Require express
+const app = express();
+//Create a new express app;
+const port = process.env.NODE_ENV == 'development' ? 3000 : 80;
+
+app.listen(port, (err)=>{
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("Server started on port ", port);
+});
+
+app.get('/', (req,res)=>{
+    res.send('hello there!');
+} );
